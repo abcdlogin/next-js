12b830edb4f2b45c22d8d29653d1590410935385
abcdlogin <82993948+abcdlogin@users.noreply.github.com>
2021-04-22 15:07:44 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+    h1{
+        color: lightseagreen;
+    }
+    h2{
+        color: white
+    }
+    body{
+        background-color: lightgreen;
+        background-image: url('http://localhost:63342/MyselfProject/me.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    </style>
+</head>
+<body>
+<h1>Home Page</h1>
+<h2>This is the home page of my site.</h2>
+<br>
+<a href="About.html">About</a>
+<a href="Hobbies.html">Hobbies</a>
+<a href="Foods.html">Foods</a>
+<a href="Skills.html">Skills</a>
+<a href="Future.html">Future</a>
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+</body>
+</html>
+
