e93747877bfeee15118369d243ecdec6157324dd
abcdlogin <82993948+abcdlogin@users.noreply.github.com>
2021-04-22 15:19:49 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to My Page!</a>
+          Welcome to My Page!
         </h1>
 
         <p className={styles.description}>
