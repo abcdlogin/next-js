ea6794d0045525a0bfbd0114ad7fa6ed3fcba537
abcdlogin <82993948+abcdlogin@users.noreply.github.com>
2021-04-22 17:34:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        
         <title>Home Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
